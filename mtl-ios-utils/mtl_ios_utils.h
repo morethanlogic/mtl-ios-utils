71f6794a6e09a46166b00723f0b060295cab10d1
Elie Zananiri <ez@silentlycrashing.net>
2012-03-14 14:12:54 -0400

    Initial Commit

---
+++ mtl-ios-utils/mtl_ios_utils.h
@@ -0,0 +1,13 @@
+//
+//  mtl_ios_utils.h
+//  mtl-ios-utils
+//
+//  Created by Elie Zananiri on 12-03-14.
+//  Copyright (c) 2012 Departement. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mtl_ios_utils : NSObject
+
+@end
---
+++ mtl-ios-utils/mtl_ios_utils.m
@@ -0,0 +1,13 @@
+//
+//  mtl_ios_utils.m
+//  mtl-ios-utils
+//
+//  Created by Elie Zananiri on 12-03-14.
+//  Copyright (c) 2012 Departement. All rights reserved.
+//
+
+#import "mtl_ios_utils.h"
+
+@implementation mtl_ios_utils
+
+@end
