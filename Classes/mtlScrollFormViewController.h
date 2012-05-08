9be7f41c765550d81c917f09468a6a552d1672fe
Elie Zananiri <ez@silentlycrashing.net>
2012-05-07 22:10:05 -0400

    Cleaned up.

--- Classes/mtlScrollFormViewController.h
+++ Classes/mtlScrollFormViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
+//--------------------------------------------------------------
+//--------------------------------------------------------------
 @interface mtlScrollFormViewController : UIViewController <UITextFieldDelegate>
 {
     UIScrollView *scrollView;
--- Classes/mtlStringValidation.h
+++ Classes/mtlStringValidation.h
@@ -8,6 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
+//--------------------------------------------------------------
+//--------------------------------------------------------------
 @interface StringValidation : NSObject
 
 + (BOOL)validateNotEmpty:(NSString *)candidate;
